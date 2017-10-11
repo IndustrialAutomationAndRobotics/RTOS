8d55713867071ff06fffa87fddbfaff06226e367
HazimGharib <hazimgharib21@gmail.com>
2017-10-11 10:14:07 +0800

    update

--- runningLED/Main_Prog.c
+++ runningLED/Main_Prog.c
@@ -23,6 +23,13 @@ int main()
 
 	xTaskCreate(LED,NULL,STACK_SIZE, NULL, 0, NULL);
 	
+	
+		
+
+	DDRD = 0xFF;
+
+	xTaskCreate(MOTOR,NULL,STACK_SIZE, NULL, 0, NULL);
+	
 	vTaskStartScheduler();
 		
 
@@ -34,38 +41,178 @@ void LED(void *pvParameters){
 
 	while(1){
 	
-		PORTB |= (1<<PB0); //Set bit to high
-		vTaskDelay(50);
+		PORTB &= (1<<PB0); //Set bit to high
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB0); // Set bit to low
 		
-		PORTB |= (1<<PB1); //Set bit to high
-		vTaskDelay(50);
+		PORTB &= (1<<PB1); //Set bit to high
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB1); // Set bit to low
 		
-		PORTB |= (1<<PB2); //Set bit to high
-		vTaskDelay(50);
+		PORTB &= (1<<PB2); //Set bit to high
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB2); // Set bit to low
 		
 		PORTB |= (1<<PB3); //Set bit to high
-		vTaskDelay(50);
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB3); // Set bit to low
 		
 		PORTB |= (1<<PB4); //Set bit to high
-		vTaskDelay(50);
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB4); // Set bit to low
+		
+		PORTB &= (1<<PB5); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB5); // Set bit to low
+		
+		PORTB &= (1<<PB6); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB6); // Set bit to low
+		
+		PORTB &= (1<<PB7); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB7); // Set bit to low
+		
+
+		
+		PORTB &= (1<<PB0); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB0); // Set bit to low
+		
+		PORTB &= (1<<PB1); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB1); // Set bit to low
+		
+		PORTB |= (1<<PB2); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB2); // Set bit to low
+		
+		PORTB &= (1<<PB3); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB3); // Set bit to low
+		
+		PORTB &= (1<<PB4); //Set bit to high
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB4); // Set bit to low
 		
 		PORTB |= (1<<PB5); //Set bit to high
-		vTaskDelay(50);
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB5); // Set bit to low
 		
+		PORTB &= (1<<PB6); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB6); // Set bit to low
+		
+		PORTB &= (1<<PB7); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB7); // Set bit to low
+
+		
+		PORTB &= (1<<PB0); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB0); // Set bit to low
+		
+		PORTB |= (1<<PB1); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB1); // Set bit to low
+		
+		PORTB &= (1<<PB2); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB2); // Set bit to low
+		
+		PORTB &= (1<<PB3); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB3); // Set bit to low
+		
+		PORTB &= (1<<PB4); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB4); // Set bit to low
+		
+		PORTB &= (1<<PB5); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB5); // Set bit to low
+	
 		PORTB |= (1<<PB6); //Set bit to high
-		vTaskDelay(50);
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB6); // Set bit to low
+		
+		PORTB &= (1<<PB7); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB7); // Set bit to low
+
+		
+		PORTB |= (1<<PB0); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB0); // Set bit to low
+		
+		PORTB &= (1<<PB1); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB1); // Set bit to low
+		
+		PORTB &= (1<<PB2); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB2); // Set bit to low
+		
+		PORTB &= (1<<PB3); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB3); // Set bit to low
+		
+		PORTB &= (1<<PB4); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB4); // Set bit to low
+		
+		PORTB &= (1<<PB5); //Set bit to high
+		vTaskDelay(20);
+		PORTB &= ~(1<<PB5); // Set bit to low
+		
+		PORTB &= (1<<PB6); //Set bit to high
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB6); // Set bit to low
 		
 		PORTB |= (1<<PB7); //Set bit to high
-		vTaskDelay(50);
+		vTaskDelay(20);
 		PORTB &= ~(1<<PB7); // Set bit to low
 		
+
+void MOTOR(void *pvParameters){
+
+while(1){
+
+		// Rotates Motor AntiClockwise
+		PORTD = 0x01; //00000001
+		vTaskDelay(100);
+
+		// Stops Motor
+		PORTD = 0x00; //00000000
+		vTaskDelay(100);
+
+		// Rotates Motor Clockwise
+		PORTD = 0x02; //00000010
+		vTaskDelay(100);
+
+		// Stops Motor
+		PORTD = 0x03; //00000011
+		vTaskDelay(100);
+
+	
+
+	
+   		// Rotates Motor AntiClockwise
+		PORTD = 0x01; //00000001
+		vTaskDelay(100);
+
+		// Stops Motor
+		PORTD = 0x00; //00000000
+		vTaskDelay(100);
+
+		// Rotates Motor Clockwise
+		PORTD = 0x02; //00000010
+		vTaskDelay(100);
+
+		// Stops Motor
+		PORTD = 0x03; //00000011
+		vTaskDelay(100);}
+
 
 		taskYIELD();
 	}
